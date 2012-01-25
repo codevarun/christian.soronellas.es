f0cd711f58fa4ccf9e756b19b123bd2afbf00490
Christian <csoronellas@emagister.com>
2012-01-25 08:18:35 +0100

    Fixed the PostsController

--- src/ChristianSoronellas/BlogBundle/Controller/PostsController.php
+++ src/ChristianSoronellas/BlogBundle/Controller/PostsController.php
@@ -15,7 +15,7 @@ use ChristianSoronellas\BlogBundle\Entity\Comment;
 
 /**
  * The Posts controller
- * 
+ *
  */
 class PostsController extends Controller
 {
@@ -28,13 +28,13 @@ class PostsController extends Controller
         $posts = $this->getDoctrine()
                       ->getRepository('ChristianSoronellasBlogBundle:Post')
                       ->findAll();
-        
+
         return array('posts' => $posts);
     }
-    
+
     /**
      * Renders a post
-     * 
+     *
      * @Route("/{year}/{month}/{day}/{slug}", name="post")
      * @Template()
      */
@@ -45,24 +45,24 @@ class PostsController extends Controller
         $date->setDate($year, $month, $day);
         $date->setTime(0, 0, 0);
         $post->getCreatedAt()->setTime(0, 0, 0);
-        
+
         if (!$post || $post->getCreatedAt() != $date || Post::STATE_COMPLETE != $post->getState()) {
             throw $this->createNotFoundException('The post doesn\'t exists!');
         }
-        
+
         $form = $this->createForm(new CommentType());
         if (null !== ($commentId = $this->getRequest()->get('commentTo'))) {
             $comment = new Comment();
             $comment->setParentComment($this->getDoctrine()->getRepository('ChristianSoronellasBlogBundle:Comment')->find((int) $commentId));
             $form->setData($comment);
         }
-        
+
         return array('post' => $post, 'form' => $form->createView());
     }
-    
+
     /**
      * Adds a new comment to a given post
-     * 
+     *
      * @var \ChristianSoronellas\BlogBundle\Entity\Post $post
      * @Route("/post/{slug}/comment", name="post_comment")
      * @Template("ChristianSoronellasBlogBundle:Posts:post.html.twig")
@@ -71,10 +71,10 @@ class PostsController extends Controller
     public function commentAction($slug)
     {
         $post = $this->getDoctrine()->getRepository('ChristianSoronellasBlogBundle:Post')->findOneBySlug($slug);
-        
+
         if (false === $post->getCommentsEnabled()) {
             $this->get('session')->setFlash('notice', 'Comments on this entry are disabled!');
-            
+
             return $this->redirect(
                     $this->generateUrl(
                             'post',
@@ -87,30 +87,30 @@ class PostsController extends Controller
                     )
             );
         }
-        
+
         if (false !== $post->getCommentsEnabled()) {
-            $form = $this->createForm(new LoginType())->bindRequest($this->getRequest());
+            $form = $this->createForm(new CommentType())->bindRequest($this->getRequest());
             if ($form->isValid()) {
                 // OK! Proceed to save the new comment to the database!
                 $em = $this->getDoctrine()->getEntityManager();
-                
+
                 $comment = $form->getData();
                 $comment->setPost($post);
-                
+
                 // Akismet filtering
                 if ($this->get('ornicar_akismet')->isSpam(array('comment_author' => $comment->getName(), 'comment_content' => $comment->getBody()))) {
                     $comment->setState(Comment::STATE_IS_SPAM);
                 }
-                
+
                 $em->persist($comment);
-                
+
                 $post->addComment($comment);
-                
+
                 $em->persist($post);
                 $em->flush();
-                
+
                 $this->get('session')->setFlash('notice', 'Your comment is awaiting moderation!');
-                
+
                 return $this->redirect(
                     $this->generateUrl(
                         'post',
@@ -123,7 +123,7 @@ class PostsController extends Controller
                     )
                 );
             }
-            
+
             return array('post' => $post, 'form' => $form->createView());
         }
     }
