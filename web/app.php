f7b0e24ea0f0556d804413f200176014f11d7df7
Christian Soronellas <theunic@gmail.com>
2012-11-10 15:47:38 +0100

    Tunned the application for a production deploy

--- web/app.php
+++ web/app.php
@@ -1,7 +1,7 @@
 <?php
 
 use Symfony\Component\ClassLoader\ApcClassLoader;
-use Symfony\Component\HttpFoundation\ApacheRequest;
+use Symfony\Component\HttpFoundation\Request;
 
 $loader = require_once __DIR__.'/../app/bootstrap.php.cache';
 
@@ -17,7 +17,7 @@ require_once __DIR__.'/../app/AppCache.php';
 $kernel = new AppKernel('prod', false);
 $kernel->loadClassCache();
 $kernel = new AppCache($kernel);
-$request = ApacheRequest::createFromGlobals();
+$request = Request::createFromGlobals();
 $response = $kernel->handle($request);
 $response->send();
 $kernel->terminate($request, $response);
